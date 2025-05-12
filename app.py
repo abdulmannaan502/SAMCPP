e7e9cf88ce148cf4165d2cc296a9eb7770bbc05f
Abdul Mannaan <abdulmannaan502@gmail.com>
2025-05-12 09:48:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 model = pk.load(open('model.pkl','rb'))
 
-st.header('Car Price Prediction ML Model')
+st.header("SAM's Car Price Prediction ML Model")
 
 cars_data = pd.read_csv('Cardetails.csv')
 
